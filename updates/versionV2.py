aaf21708fecb3049e50629fd68a0ee529f719d93
Alexey Merzlyakov - LeeGarChat <leegarchat@gmail.com>
2025-02-16 02:13:25 +0300

    asdasd

--- updates/versionV2.py
+++ updates/versionV2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1abd6973bf45abe5ec5aabb75a6ff22c3d912b36e2eee877c88edf7289842d1
+oid sha256:bdcc83f75b7ee6fb3db4372c1280ee7cddaa4f839d2d2aa323c3f11af6d8e35a
 size 4099
